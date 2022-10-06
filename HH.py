5824e5a753621ac9c9d935f2c87ab92896ea6de8
Eduard-Gindullin <mcp2006@yandex.ru>
2022-10-06 09:46:29 +0500

    v2

--- HH.py
+++ HH.py
@@ -1,12 +1,11 @@
 from encodings.utf_8 import encode
 from random import randint
-from wsgiref import headers
 from bs4 import BeautifulSoup
 from requests import get as reqt
 import json
-import tqdm
+
 import time
-import user_agent
+
 
 data = {
     "data":[]
@@ -16,16 +15,10 @@ def headers():
     headers = dict()
     headers['user-agent'] = 'Mozilla/5.0 (WindowsNT 10.0; Win64; x64) AppleWebKit/537.36'
     return headers
-    # 'Host': 'ufa.hh.ru'
-
-
-#rt = reqt(tor_ports=(9050,), tor_cport=9051)
-
-#def parse(url):
+#
 items = list()
     
-for page in range(0,1):
-        print(page)
+for page in range(0-40):
         page_url = f'https://hh.ru/search/vacancy?text=python+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&salary=&clusters=true&no_magic=true&ored_clusters=true&enable_snippets=true&page={page}&hhtmFrom=vacancy_search_list'
         resp = reqt(page_url, headers=headers()) 
         soup = BeautifulSoup(resp.text, "lxml")
@@ -53,6 +46,6 @@ for page in range(0,1):
                 items.append(item)
                 time.sleep(randint(1,3)) 
 
-                with open("data.json","w") as file:
-                    json.dump(items,file, ensure_ascii=False)
+                with open("data.json","w",encoding='utf-8') as jsonfile:
+                    json.dump(items,jsonfile, ensure_ascii=False)
                 
