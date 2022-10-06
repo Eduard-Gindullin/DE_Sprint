66e9036f6259e4757d523322e09b64e63834f2fb
Eduard-Gindullin <mcp2006@yandex.ru>
2022-10-06 09:22:35 +0500

    v2

--- main2.1.py
+++ main2.1.py
@@ -1,22 +1,58 @@
-for page in range(0,1):
-    time.sleep(2)
-    url = 
-    headers = {'User-Agent': }
-    resp = rt.get(url, headers=headers)
-    soup = BeautifulSoup(resp.text, "lxml")
-    tags = soup.find_all("a",class_="serp-item__title")
-    #(attrs={"data-qa":"bloko-header-3"})
-#    print(tags)
-    for iter in tqdm.tqdm(tags):
-        time.sleep(2)
-     #   links = [a['href'] for a in tags if a.has_key('href')]
-     #   url_object = "https://hh.ru/" + iter.attrs["href"]
-     #   resp_object = reqt.get(url_object)
-        
-        print(iter.text, iter.attrs['href'])
+from encodings.utf_8 import encode
+from random import randint
+from wsgiref import headers
+from bs4 import BeautifulSoup
+from requests import get as reqt
+import json
+import tqdm
+import time
+import user_agent
 
- #       soup_object = BeautifulSoup(resp_object.text, "lxml")
- #       tag_price = soup_object.find(attrs={"data-qa":"vacancy-salary-compensation-type-net"}).find_all().text
- #       tag_region = soup_object.find(attrs={"data-qa":"vacancy-serp__vacancy-address"}).find_all()[0].text
- #       tag_sal=soup_object.find(attrs={"data-qa":"vacancy-salary"}) 
- #       data["data"].append({"Title":iter.text, "Salary":tag_price, "Region":tag_region})
+data = {
+    "data":[]
+}
+
+def headers():
+    headers = dict()
+    headers['user-agent'] = 'Mozilla/5.0 (WindowsNT 10.0; Win64; x64) AppleWebKit/537.36'
+    return headers
+    # 'Host': 'ufa.hh.ru'
+
+
+#rt = reqt(tor_ports=(9050,), tor_cport=9051)
+
+#def parse(url):
+items = list()
+    
+for page in range(0,1):
+        print(page)
+        page_url = f'https://hh.ru/search/vacancy?text=python+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&salary=&clusters=true&no_magic=true&ored_clusters=true&enable_snippets=true&page={page}&hhtmFrom=vacancy_search_list'
+        resp = reqt(page_url, headers=headers()) 
+        soup = BeautifulSoup(resp.text, "lxml")
+        page_items = soup.find_all(class_="vacancy-serp-item-body")
+        
+        if len(page_items)==0:
+            break
+        for item_body in page_items:
+                link = item_body.find(attrs={"data-qa":"serp-item__title"}) 
+                item = {'title':link.text}
+
+                region = item_body.find(attrs={"data-qa":"vacancy-serp__vacancy-address"})
+                item['region'] = region and region.text
+
+                link_resp = reqt(link.attrs['href'], headers=headers())
+                soup_item = BeautifulSoup(link_resp.text, "lxml")
+
+                salary = soup_item.find(attrs={"data-qa":"vacancy-salary-compensation-type-net"})
+                item['salary'] = salary and salary.text 
+
+                experience = soup_item.find(attrs={"data-qa":"vacancy-experience"})
+                item['work experience'] = experience and experience.text
+
+                print(item)
+                items.append(item)
+                time.sleep(randint(1,3)) 
+
+                with open("data.json","w") as file:
+                    json.dump(items,file, ensure_ascii=False)
+                
